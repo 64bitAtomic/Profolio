e966ce7f98246bdb82519f7303e62dbc5ee423d0
64bitAtomic <zk286187@gmail.com>
2025-07-01 01:18:18 +0530

    packup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,3 @@
-// import "./Footer.css";
-// const Footer = () => {
-//   return (
-//     <div className="footer">
-//       © 2024 Mohammad Zaid Khan. All rights reserved.{" "}
-//       <a
-//         style={{ textDecoration: "none", color: "#fff" }}
-//         href="https://wa.me/919770869177"
-//         aria-label="Call Mohammad Zaid Khan"
-//       >
-//         <img
-//           src="./assets/images/whatsapp.png"
-//           alt="WhatsApp Logo"
-//           style={{ paddingTop: "5px", width: "25px", height: "25px" }}
-//         />
-//         +919770869177
-//       </a>
-//     </div>
-//   );
-// };
-
-// export default Footer;
-
 import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import "./Footer.css";
@@ -72,7 +49,7 @@ const Footer = () => {
           <MdEmail />
         </a>
       </div>
-      <ResumeDownloadButton/>
+      <ResumeDownloadButton />
     </div>
   );
 };
